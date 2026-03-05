43a032d1167cb6b363629af57de1265fd9aa1d6c
Angel DLT <angeldev2110@gmail.com>
2026-03-04 22:16:05 -0600

    feat: implement sass and i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,27 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+
   app: {
     head: {
       title: "Angel DLT",
     },
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        sass: {
+          additionalData: `@use "~/assets/sass/colors.sass" as *;\n@use "~/assets/sass/variables.sass" as *;`,
+        },
+      },
+    },
+  },
+  modules: ["@nuxt/fonts", "@nuxtjs/i18n"],
+  i18n: {
+    defaultLocale: "en",
+    locales: [
+      { code: "en", name: "English", file: "en.json", language: "en-US" },
+      { code: "es", name: "Español", file: "es.json", language: "es-ES" },
+    ],
   },
 });
