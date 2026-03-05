d702cdcb0a1351134189b5758f659d2bd627aabb
Angel DLT <angeldev2110@gmail.com>
2026-03-04 22:16:37 -0600

    feat: add normalize.css for consistent styling across browsers

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,24 @@
 <template>
   <h1>Welcome</h1>
 </template>
+
+<script lang="ts" setup>
+// Imports
+import "~/assets/normalize/normalize.css";
+
+// Component Options
+
+// Props and Emits
+
+// Composition API Helpers
+
+// Reactive Variables
+
+// Computed Properties
+
+// Watchers
+
+// Lifecycle Hooks
+
+// Methods
+</script>
